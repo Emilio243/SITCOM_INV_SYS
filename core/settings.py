f20a4423623ff410919a7ede52aed19adefde553
Emilio243 <emiliosaucedo24@gmail.com>
2026-07-14 18:08:33 -0400

    Secure CORS origins for Vercel

--- core/settings.py
+++ core/settings.py
@@ -59,7 +59,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://sitcom-inv-sys.vercel.app",
+]
 
 ROOT_URLCONF = 'core.urls'
 
